0bc51a7f0f1e33ec0ff2aca7e3e171549bc9b3e7
Carlos <carlos.santos110@fatec.sp.gov.br>
2018-03-05 20:06:30 -0300

    Atualizações gerais

--- scrapWikipedia.py
+++
@@ -1,5 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-page = requests.get("https://pt.wikipedia.org/wiki/Lista_de_c%C3%B3digos_de_estado_HTTP")
-soup = BeautifulSoup(page.content, 'html.parser')
